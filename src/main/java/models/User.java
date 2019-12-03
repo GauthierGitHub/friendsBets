8883a95b67671b0615a78ec82dc90a15d045c0a4
Gauthier <gauthier.emailpro@gmail.com>
2019-12-03 19:35:04 +0100

    initial commit

---
+++ src/main/java/models/User.java
@@ -0,0 +1,12 @@
+package models;
+
+import java.util.List;
+
+public class User {
+	private static int AUTO_INCREMENT = 0;
+	private static int id;
+	private String nickname;
+	private List<Group> grpList;
+	
+	
+}
