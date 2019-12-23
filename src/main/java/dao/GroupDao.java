3c04be37b77fa6f418147b3cd67efe9d69466116
GauthierGitHub <gauthier.emailpro@gmail.com>
2019-12-23 15:07:11 +0100

    delete old dao

--- src/main/java/dao/GroupDao.java
+++ src/main/java/dao/GroupDao.java
@@ -8,24 +8,4 @@ import models.User;
 
 public class GroupDao extends GenericDao<Group>{
 
-	public void saveUserInGroup(Group g, List<User> lu) throws MyException {
-		try {
-			// find the group in database and write users into
-			database.get(g.getId()).getUserList().addAll(lu);
-		} catch (Exception e) {
-			// TODO do all database exception
-			throw new MyException("GroupDao.save",e);
-		} 
-	}
-
-	public void deletUserInGroup(Group g, List<User> lu) throws MyException {
-		// TODO Auto-generated method stub
-		try {
-			// find the group in database and write users into
-			database.get(g.getId()).getUserList().removeAll(lu);
-		} catch (Exception e) {
-			// TODO do all database exception
-			throw new MyException("GroupDao.update",e);
-		} 
-	}
 }
--- src/main/java/services/GroupService.java
+++ src/main/java/services/GroupService.java
@@ -10,59 +10,5 @@ import models.Group;
 import models.User;
 
 public class GroupService {
-	
-	private GroupDao gDao = new GroupDao();
-	
-	public void createGroup(User u) {
-		// new group with u as admin
-		Group g = new Group(u);
-		// Write group in database
-		gDao.save(g);
-	}
 
-	public void addUserInGroup(Group g, List<User> lu) throws MyException {
-		// TODO only admin do in presentation view ?
-		// TODO exception if group or user not found
-		// TODO if admin leave the group
-		// TODO associative tables UserInGroup ?
-		
-		try {
-			// find the group in database
-			Group gInDb = gDao.findAll().get(g.getId());
-			
-			// check if one user in list is yet in group, if yes delete it from lu
-			// control in GUI ?
-			for(User u : gInDb.getUserList()) {
-				if(gInDb.getUserList().contains(u))
-					lu.remove(u);
-			}
-			
-			// write in db
-			gDao.saveUserInGroup(g, lu);
-		} catch (Exception e) {
-			// TODO do all exception
-			throw new MyException("addUserInGroup",e);
-		} 
-	}
-	
-	public void deleteUserInGroup(Group g, List<User> lu, User newAdmin) throws MyException {
-		
-		try {
-			// find the group in database
-			Group gInDb = gDao.findAll().get(g.getId());
-			// TODO check users in list is yet in group in GUI ?
-			gInDb.getUserList().removeAll(lu);
-			gInDb.setAdminGroup(newAdmin);
-
-			// write in db
-			gDao.deletUserInGroup(g, lu);
-		} catch (Exception e) {
-			// TODO do all exception
-			throw new MyException("addUserInGroup",e);
-		} 
-	}
-	
-	public GroupDao getgDao() {
-		return gDao;
-	}
 }
