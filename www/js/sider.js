1a24752c87b9e8aa41cab0384cfb90b2a2faf61c
GauthierGitHub <gauthier.emailpro@gmail.com>
2020-01-10 13:55:29 +0100

    css action button

--- www/js/sider.js
+++ www/js/sider.js
@@ -1,11 +1,23 @@
-$(".heading-compose").click(function() {
-    $(".side-two").css({
-      "left": "0"
-    });
+/**
+ * https://bootsnipp.com/snippets/3X2mq
+ */
+$(".heading-compose").click(function () {
+  $(".side-two").css({
+    "left": "0"
+  });
 });
 
-$(".newMessage-back").click(function() {
-    $(".side-two").css({
-      "left": "-100%"
-    });
+$(".newMessage-back").click(function () {
+  $(".side-two").css({
+    "left": "-100%"
+  });
+});
+
+/**
+ * https://bootsnipp.com/snippets/nNg98
+ */
+$(document).ready(function () {
+  $('#action_menu_btn').click(function () {
+    $('.action_menu').toggle();
+  });
 });
