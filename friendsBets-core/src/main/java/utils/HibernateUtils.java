886f2ca02bd19f15f12b8b54b6c17deaff350eaf
GauthierGitHub <gauthier.emailpro@gmail.com>
2020-01-30 16:16:50 +0100

    restructuration to modules

---
+++ friendsBets-core/src/main/java/utils/HibernateUtils.java
@@ -0,0 +1,59 @@
+package utils;
+
+import java.util.Properties;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtils {
+	private static final SessionFactory sessionFactory;
+
+	static {
+		try (var is = HibernateUtils.class.getClassLoader().getResourceAsStream("database.properties")) {
+			Properties p = new Properties();
+			p.load(is);
+			sessionFactory = new Configuration().configure().addProperties(p).buildSessionFactory();
+		} catch (Throwable ex) {
+			System.err.println("Initial SessionFactory creation failed." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
+
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+}
+//package utils;
+//
+//import java.nio.file.Files;
+//import java.nio.file.Paths;
+//import java.util.Properties;
+//
+//import org.hibernate.SessionFactory;
+//import org.hibernate.cfg.Configuration;
+//
+///**
+// * Construction en une fois de la sessionFactory
+// * @author gauthier
+// *
+// */
+//public class HibernateUtils {
+//
+//	private static final SessionFactory SESSIONFACTORY;
+//	
+//	static {
+//		try (var is = Files.newInputStream(Paths.get("src/main/resources/db.properties"))) {
+//			Properties p = new Properties();
+//			p.load(is);
+//			SESSIONFACTORY = new Configuration().configure().addProperties(p).buildSessionFactory();
+//		} catch (Throwable ex) {
+//			System.err.println("Initial SessionFactory creation failed." + ex);
+//			throw new ExceptionInInitializerError(ex);
+//		}
+//	}
+//
+//	public static SessionFactory getSessionFactory() {
+//		return SESSIONFACTORY;
+//	}
+//	
+//}
