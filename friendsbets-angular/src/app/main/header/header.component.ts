797d421f652c287b02794c4d8c3b323292dc564a
GauthierGitHub <gauthier.emailpro@gmail.com>
2020-02-21 13:38:50 +0100

    remove bootstrap / header css

---
+++ friendsbets-angular/src/app/main/header/header.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ConnectionService } from 'src/app/connection/connection.service';
+import { User } from 'src/app/models/user.model';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  u: User;
+
+  constructor(private cs: ConnectionService) {
+    this.u = this.cs.connectedUser == null ?
+         new User(-1, "fromMainCompo", "fromMainCompo", "fromMainCompo" ) : this.cs.connectedUser;
+   }
+
+  ngOnInit(): void {
+  }
+
+}
